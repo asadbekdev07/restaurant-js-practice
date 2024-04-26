f61a77543cd645f214d6e6e94fd969e8abf3772e
asadbekdev07 <futureprotrader1007@gmail.com>
2024-04-26 11:42:40 +0500

    Add OfferMenu Js Class

--- js/script.js
+++ js/script.js
@@ -147,4 +147,69 @@ window.addEventListener("DOMContentLoaded", () => {
 
   const modalTimerId = setTimeout(openModal, 5000)
 
+  // Class
+
+  class OfferMenu {
+    constructor(src, alt, title, descr, discount, sale, parentSelector) {
+      this.src = src
+      this.alt = alt
+      this.title = title
+      this.descr = descr
+      this.discount = discount
+      this.sale = sale
+      this.parent = document.querySelector(parentSelector)
+      this.formatToUSD()
+    }
+
+    formatToUSD() {
+      this.discount = this.discount.toLocaleString("en-US", {style:"currency", currency:"USD"})
+      this.sale = this.sale.toLocaleString("en-US", {style:"currency", currency:"USD"})
+    }
+
+    render() {
+      const element = document.createElement("div")
+      element.innerHTML = `
+      <img src="${this.src}" alt="${this.alt}">
+      <div>
+      <h3>${this.title}</h3>
+      <p>${this.descr}</p>
+      <p><del>${this.discount}</del> <span class="primary-text">${this.sale}</span></p>
+      </div>`
+
+      this.parent.append(element)
+    }
+  }
+
+  const offers = [
+    {
+      src: "./img/offer1.png",
+      alt: "Quattro Pasta",
+      title: "Quattro Pasta",
+      descr: "Pasta is a type of food made from a mixture of flour, eggs, and water.",
+      discount: 55,
+      sale: 20
+    },
+    {
+      src: "./img/offer2.png",
+      alt: "Vegertarian Pasta",
+      title: "Vegertarian Pasta",
+      descr: "Delicious pasta for vegertarian people.",
+      discount: 45,
+      sale: 18
+    },
+    {
+      src: "./img/offer3.png",
+      alt: "Gluten-Free Pasta",
+      title: "Gluten-Free Pasta",
+      descr: "Delicious and healthy pasta made of corn, rice, and even potato flour.",
+      discount: 50,
+      sale: 20
+    },
+  ]
+
+  offers.forEach(offer => {
+    const {src, alt, title, descr, discount, sale} = offer
+    new OfferMenu(src, alt, title, descr, discount, sale, ".offers-items").render()
+  })
+
 })
