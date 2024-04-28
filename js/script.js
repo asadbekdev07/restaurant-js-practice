ad91e13944f7cbd07c87c8cd3284199bd7370469
asadbekdev07 <futureprotrader1007@gmail.com>
2024-04-28 19:49:47 +0500

    Send message to telegram bot

--- js/script.js
+++ js/script.js
@@ -1,310 +1,306 @@
-  // Tabs
-  const tabs = document.querySelectorAll(".tabheader__item")
-  const tabParents = document.querySelector(".tabheader__items")
-  const tabContents = document.querySelectorAll(".tab_content")
+window.addEventListener("DOMContentLoaded", () => {
 
-  function hideTabContents() {
-    tabContents.forEach(tabContent => {
-      tabContent.style.display = "none"
-    })
+    // Tabs
+    const tabs = document.querySelectorAll(".tabheader__item")
+    const tabParents = document.querySelector(".tabheader__items")
+    const tabContents = document.querySelectorAll(".tab_content")
 
-    tabs.forEach(tab => {
-      tab.classList.remove("tabheader__item_active")
-    })
-  }
+    function hideTabContents() {
+      tabContents.forEach(tabContent => {
+        tabContent.style.display = "none"
+      })
 
-  function showTabContent(index = 0)  { //default index        index = 0
-    tabContents[index].style.display = "flex"
-    tabs[index].classList.add("tabheader__item_active")
-  }
-
-  hideTabContents()
-  showTabContent(0)
-
-  tabParents.addEventListener("click", event => {
-    const target = event.target
-
-    if (target && target.classList.contains("tabheader__item")) {
-      tabs.forEach((tab, index) => {
-        if (target === tab) {
-          hideTabContents()
-          showTabContent(index)
-        }
+      tabs.forEach(tab => {
+        tab.classList.remove("tabheader__item_active")
       })
     }
-  })
 
-  // Loader
-  const loaderWrapper = document.querySelector(".loader__wrapper")
-
-  setTimeout(() => {
-    loaderWrapper.style.display = "none"
-  }, 1500)
-
-
-  // Timer
-  const deadline = "2024-06-01"
-
-  function getTimeRemaining(endtime) {
-    let days, hours, minutes, seconds
-    time = Date.parse(endtime) - Date.parse(new Date())
-
-    if (time <= 0) {
-      days = 0,
-      hours = 0,
-      minutes = 0,
-      seconds = 0
-    } else {
-      days = Math.floor(time / (1000 * 60 * 60 * 24))
-      hours = Math.floor((time / (1000 * 60 * 60)) % 24)
-      minutes = Math.floor(time / (1000 * 60) % 60)
-      seconds = Math.floor((time / 1000) % 60)
+    function showTabContent(index = 0)  { //default index        index = 0
+      tabContents[index].style.display = "flex"
+      tabs[index].classList.add("tabheader__item_active")
     }
 
-    return {
-      totalTime: time,
-      days,
-      hours,
-      minutes,
-      seconds,
-    }
-  }
+    hideTabContents()
+    showTabContent(0)
 
-  function formatNumber(number) {
-    if (number >= 0 && number < 10) {
-      return `0${number}`
-    } else {
-      return number
-    }
-  }
+    tabParents.addEventListener("click", event => {
+      const target = event.target
 
+      if (target && target.classList.contains("tabheader__item")) {
+        tabs.forEach((tab, index) => {
+          if (target === tab) {
+            hideTabContents()
+            showTabContent(index)
+          }
+        })
+      }
+    })
 
-  function setClock(selector, endtime) {
-    const timer = document.querySelector(selector)
-    const days = timer.querySelector("#days")
-    const hours = timer.querySelector("#hours")
-    const minutes = timer.querySelector("#minutes")
-    const seconds = timer.querySelector("#seconds")
-    const timeInterval = setInterval(updateClock, 1000)
+    // Loader
+    const loaderWrapper = document.querySelector(".loader__wrapper")
 
-    updateClock()
-    function updateClock() {
-      const time = getTimeRemaining(endtime)
-
-      days.innerHTML = formatNumber(time.days)
-      hours.innerHTML = formatNumber(time.hours)
-      minutes.innerHTML = formatNumber(time.minutes)
-      seconds.innerHTML = formatNumber(time.seconds)
+    setTimeout(() => {
+      loaderWrapper.style.display = "none"
+    }, 1500)
 
 
-      if (time.totalTime <= 0) {
-        clearInterval(timeInterval)
+    // Timer
+    const deadline = "2024-06-01"
+
+    function getTimeRemaining(endtime) {
+      let days, hours, minutes, seconds
+      time = Date.parse(endtime) - Date.parse(new Date())
+
+      if (time <= 0) {
+        days = 0,
+        hours = 0,
+        minutes = 0,
+        seconds = 0
+      } else {
+        days = Math.floor(time / (1000 * 60 * 60 * 24))
+        hours = Math.floor((time / (1000 * 60 * 60)) % 24)
+        minutes = Math.floor(time / (1000 * 60) % 60)
+        seconds = Math.floor((time / 1000) % 60)
+      }
+
+      return {
+        totalTime: time,
+        days,
+        hours,
+        minutes,
+        seconds,
       }
     }
-  }
 
-  setClock(".timer", deadline)
-
-  // Modal
-  const modalOpenBtn = document.querySelectorAll("[data-modal]")
-  const modal = document.querySelector(".modal")
-  const modalCloseBtn = document.querySelector("[data-modal-close]")
-  const modalContent = document.querySelector(".modal__content")
-
-  function openModal() {
-    modalContent.classList.add("modal-fade")
-    modal.classList.add("show")
-    modal.classList.remove("hide")
-    document.body.style.overflow = "hidden"
-    clearInterval(modalTimerId)
-  }
-
-  function closeModal() {
-    modal.classList.add("hide")
-    modal.classList.remove("show")
-    document.body.style.overflow = ""
-  }
-
-  modalOpenBtn.forEach(item => {
-    item.addEventListener("click", openModal)
-  })
-
-  modalCloseBtn.addEventListener("click", closeModal)
-
-  modal.addEventListener("click", (evt) => {
-    if (evt.target === modal) {
-      closeModal()
-    }
-  })
-
-  document.addEventListener("keydown", event => {
-    if (event.code === "Escape" && modal.classList.contains("show")) {
-      closeModal()
-    }
-  })
-
-  const modalTimerId = setTimeout(openModal, 5000)
-
-  // Class
-
-  class OfferMenu {
-    constructor(src, alt, title, descr, discount, sale, parentSelector) {
-      this.src = src
-      this.alt = alt
-      this.title = title
-      this.descr = descr
-      this.discount = discount
-      this.sale = sale
-      this.parent = document.querySelector(parentSelector)
-      this.formatToUSD()
+    function formatNumber(number) {
+      if (number >= 0 && number < 10) {
+        return `0${number}`
+      } else {
+        return number
+      }
     }
 
-    formatToUSD() {
-      this.discount = this.discount.toLocaleString("en-US", {style:"currency", currency:"USD"})
-      this.sale = this.sale.toLocaleString("en-US", {style:"currency", currency:"USD"})
+
+    function setClock(selector, endtime) {
+      const timer = document.querySelector(selector)
+      const days = timer.querySelector("#days")
+      const hours = timer.querySelector("#hours")
+      const minutes = timer.querySelector("#minutes")
+      const seconds = timer.querySelector("#seconds")
+      const timeInterval = setInterval(updateClock, 1000)
+
+      updateClock()
+      function updateClock() {
+        const time = getTimeRemaining(endtime)
+
+        days.innerHTML = formatNumber(time.days)
+        hours.innerHTML = formatNumber(time.hours)
+        minutes.innerHTML = formatNumber(time.minutes)
+        seconds.innerHTML = formatNumber(time.seconds)
+
+
+        if (time.totalTime <= 0) {
+          clearInterval(timeInterval)
+        }
+      }
     }
 
-    render() {
-      const element = document.createElement("div")
-      element.innerHTML = `
-      <img src="${this.src}" alt="${this.alt}">
-      <div>
-      <h3>${this.title}</h3>
-      <p>${this.descr}</p>
-      <p><del>${this.discount}</del> <span class="primary-text">${this.sale}</span></p>
+    setClock(".timer", deadline)
+
+    // Modal
+    const modalOpenBtn = document.querySelectorAll("[data-modal]")
+    const modal = document.querySelector(".modal")
+    const modalContent = document.querySelector(".modal__content")
+
+    function openModal() {
+      modalContent.classList.add("modal-fade")
+      modal.classList.add("show")
+      modal.classList.remove("hide")
+      document.body.style.overflow = "hidden"
+      clearInterval(modalTimerId)
+    }
+
+    function closeModal() {
+      modal.classList.add("hide")
+      modal.classList.remove("show")
+      document.body.style.overflow = ""
+    }
+
+    modalOpenBtn.forEach(item => {
+      item.addEventListener("click", openModal)
+    })
+
+    modal.addEventListener("click", (evt) => {
+      if (evt.target === modal || event.target.getAttribute("data-modal-close") === "") {
+        closeModal()
+      }
+    })
+
+    document.addEventListener("keydown", event => {
+      if (event.code === "Escape" && modal.classList.contains("show")) {
+        closeModal()
+      }
+    })
+
+    const modalTimerId = setTimeout(openModal, 50000)
+
+    // Class
+
+    class OfferMenu {
+      constructor(src, alt, title, descr, discount, sale, parentSelector) {
+        this.src = src
+        this.alt = alt
+        this.title = title
+        this.descr = descr
+        this.discount = discount
+        this.sale = sale
+        this.parent = document.querySelector(parentSelector)
+        this.formatToUSD()
+      }
+
+      formatToUSD() {
+        this.discount = this.discount.toLocaleString("en-US", {style:"currency", currency:"USD"})
+        this.sale = this.sale.toLocaleString("en-US", {style:"currency", currency:"USD"})
+      }
+
+      render() {
+        const element = document.createElement("div")
+        element.innerHTML = `
+        <img src="${this.src}" alt="${this.alt}">
+        <div>
+        <h3>${this.title}</h3>
+        <p>${this.descr}</p>
+        <p><del>${this.discount}</del> <span class="primary-text">${this.sale}</span></p>
+        </div>`
+
+        this.parent.append(element)
+      }
+    }
+
+    const offers = [
+      {
+        src: "./img/offer1.png",
+        alt: "Quattro Pasta",
+        title: "Quattro Pasta",
+        descr: "Pasta is a type of food made from a mixture of flour, eggs, and water.",
+        discount: 55,
+        sale: 20
+      },
+      {
+        src: "./img/offer2.png",
+        alt: "Vegertarian Pasta",
+        title: "Vegertarian Pasta",
+        descr: "Delicious pasta for vegertarian people.",
+        discount: 45,
+        sale: 18
+      },
+      {
+        src: "./img/offer3.png",
+        alt: "Gluten-Free Pasta",
+        title: "Gluten-Free Pasta",
+        descr: "Delicious and healthy pasta made of corn, rice, and even potato flour.",
+        discount: 50,
+        sale: 20
+      },
+    ]
+
+    offers.forEach(offer => {
+      const {src, alt, title, descr, discount, sale} = offer
+      new OfferMenu(src, alt, title, descr, discount, sale, "#offers .offers-items").render()
+    })
+
+    const dayTimeItems = [
+      {
+        src: "./img/breckfastIcon.png",
+        alt: "Breakfast",
+        title: "Breakfast",
+        descr: "8:00 am to 10:00 am"
+      },
+      {
+        src: "./img/lunchIcon.png",
+        alt: "Lunch",
+        title: "Lunch",
+        descr: "4:00 pm to 7:00 pm"
+      },
+      {
+        src: "./img/dinnerIcon.png",
+        alt: "Dinner",
+        title: "Dinner",
+        descr: "9:00 pm to 1:00 Am"
+      },
+      {
+        src: "./img/dessertIcon.png",
+        alt: "Desert",
+        title: "Desert",
+        descr: "All time"
+      },
+    ]
+
+    dayTimeItems.forEach(dayTimeItem => {
+      const {src, alt, title, descr,} = dayTimeItem
+      new OfferMenu(src, alt, title, descr, "", "", ".daytime-items").render()
+    })
+
+
+    // FORM
+    const form = document.querySelector("form")
+    const telegramTokenBot = "6913787807:AAFBNLiHn-ysNkKEh-bCjIiWCwBDIiqdVLY"
+    const chatId = "6726160029"
+    const message = {
+      loading: "Loading...",
+      success: "Thanks for contacting with us",
+      failure: "Something went wrong"
+    }
+
+    form.addEventListener("submit", event => {
+      event.preventDefault()
+
+      const loader = document.createElement("div")
+      loader.classList.add("loader")
+      form.append(loader)
+
+      const formData = new FormData(form)
+
+      const object = {}
+      formData.forEach((value, key) => {
+        object[key] = value
+      })
+
+
+      fetch(`https://api.telegram.org/bot${telegramTokenBot}/sendMessage`, {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+          chat_id: chatId,
+          text: `name: ${object.name}, Phone: ${object.phone}`
+        }),
+      })
+      .then(() => {showStatusMessage(message.success), form.reset()})
+      .catch(() => {showStatusMessage(message.failure)})
+      .finally(() => loader.remove())
+    })
+
+    function showStatusMessage(message) {
+      const modalDialog = document.querySelector(".modal__dialog")
+
+      modalDialog.classList.add("hide")
+      openModal()
+
+      const statusModal = document.createElement("div")
+      statusModal.classList.add("modal__dialog")
+      statusModal.innerHTML = `
+      <div class="modal__content">
+        <div data-modal-close class="modal__close">&times;</div>
+        <div data-modal-close class="modal__close">&times;</div>
+          <div class="modal__title">${message}</div>
       </div>`
 
-      this.parent.append(element)
+      document.querySelector(".modal").append(statusModal)
+
+      setTimeout(() => {
+        statusModal.remove()
+        modalDialog.classList.remove("hide")
+        closeModal()
+      }, 4000)
     }
-
-    renderDayTimeItems() {
-      const elDayTimeItem = document.createElement("div")
-      elDayTimeItem.innerHTML = `
-      <img src="${this.src}" alt="${this.alt}">
-      <div>
-      <h3>${this.title}</h3>
-      <p>${this.descr}</p>
-      </div>`
-
-      this.parent.append(elDayTimeItem)
-    }
-  }
-
-  const offers = [
-    {
-      src: "./img/offer1.png",
-      alt: "Quattro Pasta",
-      title: "Quattro Pasta",
-      descr: "Pasta is a type of food made from a mixture of flour, eggs, and water.",
-      discount: 55,
-      sale: 20
-    },
-    {
-      src: "./img/offer2.png",
-      alt: "Vegertarian Pasta",
-      title: "Vegertarian Pasta",
-      descr: "Delicious pasta for vegertarian people.",
-      discount: 45,
-      sale: 18
-    },
-    {
-      src: "./img/offer3.png",
-      alt: "Gluten-Free Pasta",
-      title: "Gluten-Free Pasta",
-      descr: "Delicious and healthy pasta made of corn, rice, and even potato flour.",
-      discount: 50,
-      sale: 20
-    },
-  ]
-
-  offers.forEach(offer => {
-    const {src, alt, title, descr, discount, sale} = offer
-    new OfferMenu(src, alt, title, descr, discount, sale, "#offers .offers-items").render()
-  })
-
-  const dayTimeItems = [
-    {
-      src: "./img/breckfastIcon.png",
-      alt: "Breakfast",
-      title: "Breakfast",
-      descr: "8:00 am to 10:00 am"
-    },
-    {
-      src: "./img/lunchIcon.png",
-      alt: "Lunch",
-      title: "Lunch",
-      descr: "4:00 pm to 7:00 pm"
-    },
-    {
-      src: "./img/dinnerIcon.png",
-      alt: "Dinner",
-      title: "Dinner",
-      descr: "9:00 pm to 1:00 Am"
-    },
-    {
-      src: "./img/dessertIcon.png",
-      alt: "Desert",
-      title: "Desert",
-      descr: "All time"
-    },
-  ]
-
-  dayTimeItems.forEach(dayTimeItem => {
-    const {src, alt, title, descr,} = dayTimeItem
-    new OfferMenu(src, alt, title, descr, "", "", ".daytime-items").render()
-  })
-
-  // class DayTimeItem {
-  //   constructor(src, alt, title, descr, parentSelector) {
-  //     this.src = src
-  //     this.alt = alt
-  //     this.title = title
-  //     this.descr = descr
-  //     this.parent = document.querySelector(parentSelector)
-  //     this.render()
-  //   }
-  //   render() {
-  //     const element = document.createElement("div")
-  //     element.classList.add("daytime-item")
-  //     element.innerHTML = `
-  //     <img src="${this.src}" alt="${this.alt}">
-  //     <div>
-  //     <h3>${this.title}</h3>
-  //     <p>${this.descr}</p>
-  //     </div>`;
-
-  //     this.parent.append(element)
-  //   }
-  // }
-
-  // const dayTimeItems = [
-  //   {
-  //     src: "./img/breckfastIcon.png",
-  //     alt: "Breakfast",
-  //     title: "Breakfast",
-  //     descr: "8:00 am to 10:00 am"
-  //   },
-  //   {
-  //     src: "./img/lunchIcon.png",
-  //     alt: "Lunch",
-  //     title: "Lunch",
-  //     descr: "4:00 pm to 7:00 pm"
-  //   },
-  //   {
-  //     src: "./img/dinnerIcon.png",
-  //     alt: "Dinner",
-  //     title: "Dinner",
-  //     descr: "9:00 pm to 1:00 Am"
-  //   },
-  //   {
-  //     src: "./img/dessertIcon.png",
-  //     alt: "Desert",
-  //     title: "Desert",
-  //     descr: "All time"
-  //   },
-  // ]
-
-  // dayTimeItems.forEach(dayTimeItem => {
-  //   const { src, alt, title, descr } = dayTimeItem;
-  //   new DayTimeItem(src, alt, title, descr, ".daytime-items");
-  // })
+})
